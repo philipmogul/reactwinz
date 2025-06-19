951ffd9422955a6a7a2a91d3b99f548b3b5cb8bf
philipvalue <philipp1mogul@gmail.com>
2025-06-19 17:57:04 +0300

    Working With Components in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+
 
 function App() {
+  return ( /* This allows inclusion of multiple html components */
+    <>
+    <Text />
+    <MyName />
+    </>
+  );
+}
+
+// Creating components in React
+// Text Component 
+function Text(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to My App</h1>
+      <p>This is a simple React application.</p>
     </div>
   );
 }
+
+// My Name Component
+function MyName() {
+  return (
+    <div>
+      <h2>My Name is PHILIP VALUE!</h2>
+      <p>I am a software developer.</p>
+    </div>
+  );
+}
+
 
 export default App;
