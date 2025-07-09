10fc8f2c1743c162d35c73c94926abb080fb4809
philipvalue <philipp1mogul@gmail.com>
2025-07-09 16:52:08 +0300

    OAuth & 2-Factor-Authentication

---
+++ src/oauth/Oauth.jsx
@@ -0,0 +1,22 @@
+function Oauth() {
+  return (
+    <div>
+      <h1>OAuth</h1>
+      <p>This is the OAuth page.</p>
+      <p>Sign in by: 
+        <ul>
+            <li>Google</li>
+            <li>Apple</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>GitHub</li>
+            <li>LinkedIn</li>
+            <li>Microsoft</li>
+            <li>Instagram</li>
+        </ul>
+      </p>
+    </div>
+  );
+}
+
+export default Oauth;
---
+++ src/oauth/TwoFactorAuthentication.jsx
@@ -0,0 +1,11 @@
+function TwoFactorAuthentication() {
+  return (
+    <div>
+      <h2>Two-Factor Authentication</h2>
+      <p>Enable two-factor authentication for added security.</p>
+      {/* Add your two-factor authentication logic here */}
+    </div>
+  );
+}
+
+export default TwoFactorAuthentication;
